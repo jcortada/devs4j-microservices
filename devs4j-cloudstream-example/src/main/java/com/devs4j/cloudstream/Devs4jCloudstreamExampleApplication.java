6d62dcacdb8a8859eaf14f91afcd4be02c8823cd
Cortada Coromina, Javier <jcortada@minsait.com>
2023-01-03 19:27:11 +0100

    first function

--- devs4j-cloudstream-example/src/main/java/com/devs4j/cloudstream/Devs4jCloudstreamExampleApplication.java
+++ devs4j-cloudstream-example/src/main/java/com/devs4j/cloudstream/Devs4jCloudstreamExampleApplication.java
@@ -1,11 +1,19 @@
 package com.devs4j.cloudstream;
 
+import java.util.function.Function;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Devs4jCloudstreamExampleApplication {
 
+	@Bean
+	public Function<String, String> toUpperCase() {
+		return String::toUpperCase;
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Devs4jCloudstreamExampleApplication.class, args);
 	}
