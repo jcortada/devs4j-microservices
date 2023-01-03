cbb85d4a5699b9d3c880bff20a9ee67bd0c803ee
Cortada Coromina, Javier <jcortada@minsait.com>
2023-01-03 19:15:19 +0100

    initial commit

---
+++ devs4j-cloudstream-example/src/main/java/com/devs4j/cloudstream/Devs4jCloudstreamExampleApplication.java
@@ -0,0 +1,13 @@
+package com.devs4j.cloudstream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Devs4jCloudstreamExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Devs4jCloudstreamExampleApplication.class, args);
+	}
+
+}
