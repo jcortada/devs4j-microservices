724982ba7e103579c9b224a240f8ee641e6a0a1e
Cortada Coromina, Javier <jcortada@minsait.com>
2023-01-04 13:21:26 +0100

    added producer,processor,consumer

--- devs4j-cloudstream-example/src/main/java/com/devs4j/cloudstream/Devs4jCloudstreamExampleApplication.java
+++ devs4j-cloudstream-example/src/main/java/com/devs4j/cloudstream/Devs4jCloudstreamExampleApplication.java
@@ -1,14 +1,52 @@
 package com.devs4j.cloudstream;
 
+import java.time.Duration;
+import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Supplier;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import reactor.core.publisher.Flux;
+
 @SpringBootApplication
 public class Devs4jCloudstreamExampleApplication {
 
+	
+	private static final Logger log = LoggerFactory.getLogger(Devs4jCloudstreamExampleApplication.class);
+
+	
+	/*
+	 * producer-out-0
+	 */
+	@Bean
+	public Supplier<Flux<Long>> producer() {
+		return () -> Flux.interval(Duration.ofSeconds(1)).log();
+	}
+	
+	/*
+	 * processor-in-0
+	 * processor-out-0
+	 */
+	@Bean
+	public Function<Flux<Long>, Flux<Long>> processor() {
+		return flx -> flx.map(nmbr -> nmbr * nmbr);
+	}
+	
+	/*
+	 * consumer-in-0
+	 */
+	@Bean
+	public Consumer<Long> consumer() {
+		return (number) -> {
+			log.info("message received {}", number);
+		};
+	}
+	
 	@Bean
 	public Function<String, String> toUpperCase() {
 		return String::toUpperCase;
